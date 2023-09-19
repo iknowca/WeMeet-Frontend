905f446f8928af81587b5548b71df721f2707be7
iknowca <iknowca@gmail.com>
2023-09-19 15:26:05 +0900

    feat: [김원기] 이벤트 배너 UI 구현 [WMF-43]

--- src/components/main/mainComp/EventBanner.vue
+++ src/components/main/mainComp/EventBanner.vue
@@ -1,8 +1,28 @@
 <template>
-<div></div>
+  <Carousel :wrap-around="true" style="width: 1000px" transition="600">
+    <Slide v-for="slide in boards" :key="slide">
+      <img :src="'https://vue-s3-test-37373.s3.ap-northeast-2.amazonaws.com/'+slide.additionalInfo.imagePath">
+    </Slide>
+    <template #addons>
+      <Navigation/>
+      <Pagination/>
+    </template>
+  </Carousel>
 </template>
 
 <script setup>
+import {onMounted, reactive} from "vue";
+import 'vue3-carousel/dist/carousel.css'
+import {Carousel, Navigation, Pagination, Slide} from 'vue3-carousel'
+import axiosInstance from "@/utility/axiosInstance";
+
+const boards = reactive([])
+onMounted(() => {
+  axiosInstance.springAxiosInst.get("/board/list/event/main")
+    .then((res)=> {
+      boards.push(...res.data)
+    })
+})
 </script>
 
 <style scoped>
