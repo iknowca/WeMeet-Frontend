901abb76e0df89d5ca8d9116a2bbe4e0ed2a507f
iknowca <iknowca@gmail.com>
2023-09-19 17:19:59 +0900

    add: [김원기] 빠진 파일 추가 [WMF-43]

--- src/components/main/MainForm.vue
+++ src/components/main/MainForm.vue
@@ -3,11 +3,17 @@
   <v-col>
     <v-row>
       <v-spacer></v-spacer>
-      <v-col cols="8">
-  <suspense>
-  <RecentMoim></RecentMoim>
-  </suspense>
+      <v-col>
+        <v-row>
+          <EventBanner></EventBanner>
+        </v-row>
+        <v-row>
+          <suspense>
+            <RecentMoim></RecentMoim>
+          </suspense>
+        </v-row>
       </v-col>
+
       <v-spacer></v-spacer>
     </v-row>
   </v-col>
@@ -16,10 +22,11 @@
 
 <script>
 import RecentMoim from "@/components/main/mainComp/RecentMoim.vue";
+import EventBanner from "@/components/main/mainComp/EventBanner.vue";
 
 export default {
   name: "MainForm",
-  components: {RecentMoim}
+  components: {EventBanner, RecentMoim}
 }
 </script>
 
